bb9769e383a9bc682531e4802492022b3d03c8a1
Nicolas F <nicolas.farreaut@gmail.com>
2023-09-13 20:19:22 -0300

    Ejercicio resuelto con for, if y %

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿for (int i = 1; i <= 100; i++)
+{
+  if (i % 3 == 0)
+  {
+    if (i % 5 == 0)
+    {
+      Console.WriteLine($"{i} - FizzBuzz");
+    }
+    else 
+    {
+      Console.WriteLine($"{i} - Fizz");
+    }
+    
+  }
+  else 
+  {
+    if (i % 5 == 0)
+    {
+      Console.WriteLine($"{i} - Buzz");
+    }
+    else 
+    {
+      Console.WriteLine(i);
+    }
+  }
+}
